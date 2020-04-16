59ab42f8001f4c45e90b89b12197f12f751c25e2
Glen Beane <glen.beane@jax.org>
2020-04-16 00:34:15 -0400

    simplify ManualLabelWidget.paintEvent(), mainly get rid of manual padding inside widget. Fix a range issue.

--- src/ui/manual_label_widget.py
+++ src/ui/manual_label_widget.py
@@ -17,7 +17,7 @@ class ManualLabelWidget(QWidget):
     _NOT_BEHAVIOR_COLOR = QColor(0, 0, 128)
 
     def __init__(self):
-        super().__init__()
+        super(ManualLabelWidget, self).__init__()
 
         # allow widget to expand horizontally but maintain fixed vertical size
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
@@ -25,6 +25,7 @@ class ManualLabelWidget(QWidget):
         # number of frames on each side of current frame to include in
         # sliding window
         self._window_size = 100
+        self._frames_to_draw = self._window_size * 2 + 1
 
         # current position
         self._current_frame = 0
@@ -35,10 +36,7 @@ class ManualLabelWidget(QWidget):
         # TrackLabels object containing labels for current behavior & identity
         self._labels = None
 
-        # positioning
-        self._padding_left = 10
-        self._padding_top = 15
-        self._bar_height = 50
+        self._bar_height = 40
 
     def sizeHint(self):
         """
@@ -47,7 +45,7 @@ class ManualLabelWidget(QWidget):
         horizontally to fill the available container. The height is fixed,
         so the value used here sets the height of the widget.
         """
-        return QSize(400, 80)
+        return QSize(400, self._bar_height)
 
     def paintEvent(self, event):
         """
@@ -56,15 +54,11 @@ class ManualLabelWidget(QWidget):
         This draws the widget.
         TODO: this could could be broken up into a few logical steps
         """
-        # figure out some dimensions
         # width of entire widget
         width = self.size().width()
 
-        # width of the bar that will be rawn
-        bar_width = width - (2 * self._padding_left)
-
         # size each frame takes up in the bar in pixels
-        pixels_per_frame = bar_width / (self._window_size * 2 + 1)
+        pixels_per_frame = width / self._frames_to_draw
 
         # starting and ending frames of the current view
         start = self._current_frame - self._window_size
@@ -72,7 +66,7 @@ class ManualLabelWidget(QWidget):
 
         # slice size for grabbing label blocks
         slice_start = max(start, 0)
-        slice_end = self._current_frame + self._window_size + 1
+        slice_end = self._current_frame + self._window_size
 
         label_blocks = self._labels.get_slice_blocks(slice_start, slice_end)
 
@@ -84,9 +78,7 @@ class ManualLabelWidget(QWidget):
         if start < 0:
             start_padding_width = abs(start) * pixels_per_frame
             qp.setBrush(QBrush(self._BACKGROUND_COLOR, Qt.Dense6Pattern))
-            qp.drawRect(self._padding_left, self._padding_top,
-                        start_padding_width,
-                        self._bar_height)
+            qp.drawRect(0, 0, start_padding_width, self._bar_height)
 
         # draw end padding if any
         end_padding_frames = 0
@@ -95,15 +87,14 @@ class ManualLabelWidget(QWidget):
             end_padding_width = end_padding_frames * pixels_per_frame
             qp.setBrush(QBrush(self._BACKGROUND_COLOR, Qt.Dense6Pattern))
             qp.drawRect(
-                self._padding_left + (self._window_size * 2 + 1 - end_padding_frames) * pixels_per_frame,
-                self._padding_top, end_padding_width, self._bar_height
+                (self._frames_to_draw - end_padding_frames) * pixels_per_frame,
+                0, end_padding_width, self._bar_height
             )
 
         # draw background color (will be color for no label)
         qp.setBrush(self._BACKGROUND_COLOR)
-        qp.drawRect(self._padding_left + start_padding_width,
-                    self._padding_top,
-                    bar_width - start_padding_width - (end_padding_frames * pixels_per_frame),
+        qp.drawRect(start_padding_width, 0,
+                    width - start_padding_width - (end_padding_frames * pixels_per_frame),
                     self._bar_height)
 
         # draw label blocks
@@ -114,11 +105,9 @@ class ManualLabelWidget(QWidget):
                 qp.setBrush(self._NOT_BEHAVIOR_COLOR)
 
             block_width = (block['end'] - block['start'] + 1) * pixels_per_frame
-            offset_x = self._padding_left + start_padding_width + block[
-                'start'] * pixels_per_frame
+            offset_x = start_padding_width + block['start'] * pixels_per_frame
 
-            qp.drawRect(offset_x, self._padding_top, block_width,
-                        self._bar_height)
+            qp.drawRect(offset_x, 0, block_width, self._bar_height)
 
         # highlight current selection
         if self._selection_start is not None:
@@ -140,23 +129,20 @@ class ManualLabelWidget(QWidget):
                 selection_start = self._current_frame - start
                 selection_width = pixels_per_frame
 
-            qp.drawRect(self._padding_left + selection_start * pixels_per_frame ,
-                        self._padding_top,
-                        selection_width,
-                        self._bar_height)
+            qp.drawRect(selection_start * pixels_per_frame, 0,
+                        selection_width, self._bar_height)
 
         # draw current position indicator
-        pen = QPen(self._POSITION_MARKER_COLOR, 2, Qt.SolidLine)
+        pen = QPen(self._POSITION_MARKER_COLOR, 1, Qt.SolidLine)
         qp.setPen(pen)
-        position_offset = width // 2
-        qp.drawLine(position_offset, self._padding_top + 1, position_offset,
-                    self._padding_top + 49)
+        position_offset = width / 2 - 1  # midpoint of widget in pixels
+        qp.drawLine(position_offset, 0, position_offset, self._bar_height - 1)
 
         # draw bounding box
         qp.setPen(self._OUTLINE_COLOR)
         qp.setBrush(Qt.NoBrush)
-        qp.drawRect(self._padding_left, self._padding_top, bar_width,
-                    self._bar_height)
+        # need to adjust the width and height to account for the pen
+        qp.drawRect(0, 0, width - 1, self._bar_height - 1)
 
     def set_labels(self, labels):
         """ load label track to display """
