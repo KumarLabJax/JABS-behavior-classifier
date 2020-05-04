bf884dd04f1fea390942247bf57453db063a7a4a
Glen Beane <glen.beane@jax.org>
2020-05-04 15:32:42 -0400

    add tests for Project

---
+++ src/tests/test_project.py
@@ -0,0 +1,102 @@
+import unittest
+import shutil
+from pathlib import Path
+import json
+
+from src.labeler.project import Project
+from src.labeler.video_labels import VideoLabels
+
+
+class TestProject(unittest.TestCase):
+    """ test labeler.project.Project """
+
+    _EXISTING_PROJ_PATH = Path('test_project_with_data')
+    _FILENAMES = ['test_file_1.avi', 'test_file_2.avi']
+
+    @classmethod
+    def setUpClass(cls):
+        # create a project with a video file and annotations
+
+        cls._EXISTING_PROJ_PATH.mkdir()
+        for name in cls._FILENAMES:
+            (cls._EXISTING_PROJ_PATH / name).touch()
+
+        # create an empty project directory
+        Project(cls._EXISTING_PROJ_PATH)
+
+        # create an annotation
+        labels = VideoLabels(cls._FILENAMES[0], 10000)
+        walking_labels = labels.get_track_labels('0', 'Walking')
+        walking_labels.label_behavior(100, 200)
+        walking_labels.label_behavior(500, 1000)
+        walking_labels.label_not_behavior(1001, 2000)
+
+        # and manually place the .json file in the project directory
+        with (cls._EXISTING_PROJ_PATH / '.labeler' / 'annotations' /
+              Path(cls._FILENAMES[0]).with_suffix('.json')
+        ).open('w', newline='\n') as f:
+            json.dump(labels.as_dict(), f)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls._EXISTING_PROJ_PATH)
+
+    def test_create(self):
+        """ test creating a new empty Project """
+        project_dir = Path('test_project_dir')
+        proj = Project(project_dir)
+
+        # make sure that the empty project directory was created
+        self.assertTrue(project_dir.exists())
+
+        # make sure the .labeler directory was created
+        self.assertTrue((project_dir / '.labeler').exists())
+
+        # make sure the .labeler/annotations directory was created
+        self.assertTrue((project_dir / '.labeler' / 'annotations').exists())
+
+        # remove project dir
+        shutil.rmtree(project_dir)
+
+    def test_get_video_list(self):
+        """ get list of video files in an existing project """
+        project = Project(self._EXISTING_PROJ_PATH)
+        self.assertListEqual(project.videos, self._FILENAMES)
+
+    def test_load_annotations(self):
+        """ test loading annotations from a saved project """
+
+        project = Project(self._EXISTING_PROJ_PATH)
+
+        labels = project.load_annotation_track(self._FILENAMES[0])
+
+        with (self._EXISTING_PROJ_PATH / '.labeler' / 'annotations' /
+              Path(self._FILENAMES[0]).with_suffix('.json')
+        ).open('r') as f:
+            dict_from_file = json.load(f)
+
+        self.assertTrue(len(project.videos), 2)
+
+        # check to see that calling as_dict() on the VideoLabels object
+        # matches what was used to load the annotation track from disk
+        self.assertDictEqual(labels.as_dict(), dict_from_file)
+
+    def test_load_annotations_bad_filename(self):
+        """
+        attempt to load annotations for a file that doesn't exist, should
+        raise a ValueError
+        """
+        project = Project(self._EXISTING_PROJ_PATH)
+
+        with self.assertRaises(ValueError):
+            labels = project.load_annotation_track('bad_filename.avi')
+
+    def test_exception_creating_video_labels(self):
+        """
+        Project can't create an empty VideoLabels object for a project avi file
+        because opencv is unable to open the video to get a frame count
+        """
+        project = Project(self._EXISTING_PROJ_PATH)
+        with self.assertRaises(IOError):
+            pass
+            labels = project.load_annotation_track(self._FILENAMES[1])
