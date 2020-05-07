374d1c703bfa623c090dc0e0a0c256f576ca5430
Glen Beane <glen.beane@jax.org>
2020-05-06 21:13:25 -0400

    Revert "Revert "overlay identities when the identity selection drop down is open""
    This reverts commit 126565920a2f72ecf1690df362ec5374ee173d5e.

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,6 +1,7 @@
 from .player_widget import PlayerWidget
 from .manual_label_widget import ManualLabelWidget
 from .timeline_label_widget import TimelineLabelWidget
+from .identity_combo_box import IdentityComboBox
 from .central_widget import CentralWidget
 from .main_window import MainWindow
 
