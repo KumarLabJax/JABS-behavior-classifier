6859b8db65f4b73419bd008e5300a170dd656bc1
Glen Beane <glen.beane@jax.org>
2020-11-10 15:42:45 -0500

    load new label/bout counts when switching behaviors

--- src/ui/central_widget.py
+++ src/ui/central_widget.py
@@ -425,6 +425,9 @@ class CentralWidget(QtWidgets.QWidget):
         self._set_label_track()
         self._reset_prediction()
         self.classify_button.setEnabled(False)
+        # get label/bout counts for the current project
+        self._label_counts = self._project.label_counts(new_behavior)
+        self._bout_counts = self._project.bout_counts(new_behavior)
         self._update_label_counts()
         self._set_train_button_enabled_state()
         self._project.save_metadata({'selected_behavior': new_behavior})
