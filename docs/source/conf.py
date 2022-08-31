52ae74e45960182ec8a195f8fa8858da7be2370d
anshu957 <32687084+anshu957@users.noreply.github.com>
2022-08-31 10:48:47 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'JABS'
+copyright = '2022, Jackson Labs'
+author = 'KumarLab'
 
 release = '0.1'
 version = '0.1.0'
