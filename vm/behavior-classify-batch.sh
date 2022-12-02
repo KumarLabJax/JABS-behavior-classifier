9b0b310d7d33140e1797f62413dfdf46b1ae02b5
Brian Geuther <Brian.Geuther@jax.org>
2022-12-02 14:46:48 -0500

    Adjustments to slurm processing to conform to other pipelines. Changes: 1. removed reliance on local paths 2. added option to use pose files or videos 3. removed forcing xgboost (in case the user specifies a different classifier) 4. adjusted pose file searching to be more easily update for new pose versions

--- vm/behavior-classify-batch.sh
+++ vm/behavior-classify-batch.sh
@@ -10,39 +10,36 @@
 
 # This is a self-submitting SLURM script which can be used to classify a batch
 # of video poses on JAX's sumner cluster. This script assumes that there will be
-# a "behavior-classifier.sif" Singularity VM image in the same directory as this
-# script. You can build this VM using the "behavior-classifier-vm.def" Singularity
+# a "behavior-classifier.sif" Singularity VM image located on the cluster to use.
+# You can build this VM using the "behavior-classifier-vm.def" Singularity
 # definition in the repository or use one that has been pre-built.
 #
 # This script expects two positional command line arguments:
-# 1) a fully qualified path to an exported Rotta classifier HDF5 file
+# 1) a fully qualified path to an exported JABS classifier HDF5 file
 # 2) a fully qualified path to a batch file. A batch file is a plain
 #    text file which contains newline separated network IDs for each
-#    pose file that should be classified. Note that even though we
-#    are performing classification on poses, the batch file is
-#    expected to contain "*.avi" entries in order to conform to
-#    our network IDs. This batch file should be placed at the root
-#    directory of all poses that we want to process. So for example,
-#    if we have a bunch of poses in "/a/b/c" one of which is
-#    "/a/b/c/d/e_pose_est_v3.h5" we should have a batch file
-#    "/a/b/c/mybatch.txt" that contains a "d/e.avi" entry.
+#    pose file that should be classified. Note that one can provide
+#    either a pose file ending with a *.h5 extension or a video file
+#    with the assumption that *.avi also possesses a *_pose_est_v##.h5.
 #
 # Example script usage:
 # 
-#   ~/behavior-classify-batch.sh \
-#       /projects/kumar-lab/USERS/sheppk/temp/rearing-batch-2021-02-23-leilani/rearing-classifiers-2020-02-18/leinani-hession/Rearing_supported_training_20210216_122124.h5 \
-#       /projects/kumar-lab/USERS/sheppk/temp/rearing-batch-2021-02-23-leilani/batch.txt
+#   /projects/kumar-lab/JABS/behavior-classify-batch.sh \
+#       /projects/kumar-lab/JABS/classifiers/Rearing_supported_training_20210216_122124.h5 \
+#       /projects/kumar-lab/JABS/batches/test-batch.txt
 #
-# And if we look at the contents of batch.txt:
+# And if we look at the contents of test-batch.txt:
 #
-#   head -n 4 /projects/kumar-lab/USERS/sheppk/temp/rearing-batch-2021-02-23-leilani/batch.txt
+#   head -n 4 /projects/kumar-lab/JABS/batches/test-batch.txt
 #
 # we see:
 #
-#   LL1-B2B/2016-05-05_SPD/LL1-3_100492-F-AX27-9-42416-3-S111.avi
-#   LL1-B2B/2017-01-01_SPD/LL1-4_002105-M-AX12-5.28571428571429-42640-4-S331.avi
-#   LL1-B2B/2017-01-12_SPD/LL1-4_001144-F-F29-4-42661-4-S344.avi
-#   LL1-B2B/2016-05-23_SPD/B6J_Male_S6730806_ep3-PSY.avi
+#   /projects/kumar-lab/video-data/LL1-B2B/2016-05-05_SPD/LL1-3_100492-F-AX27-9-42416-3-S111.avi
+#   /projects/kumar-lab/video-data/LL1-B2B/2017-01-01_SPD/LL1-4_002105-M-AX12-5.28571428571429-42640-4-S331.avi
+#   /projects/kumar-lab/video-data/LL1-B2B/2017-01-12_SPD/LL1-4_001144-F-F29-4-42661-4-S344.avi
+#   /projects/kumar-lab/video-data/LL1-B2B/2016-05-23_SPD/B6J_Male_S6730806_ep3-PSY.avi
+
+CLASSIFICATION_IMG=/projects/kumar-lab/JABS/JABS-GUI-2022-10-12-FIX-V4-TEST.sif
 
 trim_sp() {
     local var="$*"
@@ -51,6 +48,29 @@ trim_sp() {
     # remove trailing whitespace characters
     var="${var%"${var##*[![:space:]]}"}"
     echo -n "$var"
+}
+
+# If the provided file ends with the extension, return itself
+# Otherwise, search in descending order
+MAX_POSE_VERSION=5
+MIN_POSE_VERSION=2
+find_pose_file() {
+    local in_file="$*"
+    if [[ "${in_file##*.}" == 'h5' ]]; then
+        echo -n ${in_file}
+    else
+        cur_pose_version=${MAX_POSE_VERSION}
+        prefix="${in_file%.*}"
+        while [[ cur_pose_version -gt ${MIN_POSE_VERSION} ]]
+        do
+            if [[ -f "${prefix}_pose_est_v${cur_pose_version}.h5" ]]; then
+                echo -n "${prefix}_pose_est_v${cur_pose_version}.h5"
+                break
+            else
+                ((cur_pose_version--))
+            fi
+        done
+    fi
 }
 
 if [[ -z "${SLURM_JOB_ID}" ]]
@@ -64,7 +84,7 @@ then
         echo "Submitting an array job for ${batch_line_count} videos"
 
         # Here we perform a self-submit
-        sbatch --export=ROOT_DIR="$(dirname "${0}")",CLASSIFIER_FILE="${1}",BATCH_FILE="${2}" --array="1-${batch_line_count}%500" "${0}"
+        sbatch --export=CLASSIFIER_FILE="${1}",BATCH_FILE="${2}" --array="1-${batch_line_count}%500" "${0}"
     else
         echo "ERROR: missing classification and/or batch file." >&2
         echo "Expected usage:" >&2
@@ -75,7 +95,7 @@ else
     # the script is being run by slurm
     if [[ -z "${SLURM_ARRAY_TASK_ID}" ]]
     then
-        echo "ERROR: no SLURM_ARRAY_TASK_ID found" >&2
+        echo "ERROR: no SLURM_ARRAY_TASK_ID found. This job should be run as an array" >&2
         exit 1
     fi
 
@@ -91,41 +111,30 @@ else
         exit 1
     fi
 
-    # here we use the array ID to pull out the right video from the batch file
-    VIDEO_FILE=$(trim_sp $(sed -n "${SLURM_ARRAY_TASK_ID}{p;q;}" < "${BATCH_FILE}"))
-    echo "BATCH VIDEO FILE: ${VIDEO_FILE}"
+    # here we use the array ID to pull out the right line from the batch file
+    BATCH_LINE=$(trim_sp $(sed -n "${SLURM_ARRAY_TASK_ID}{p;q;}" < "${BATCH_FILE}"))
+    echo "BATCH LINE FILE: ${BATCH_LINE}"
+    # Try and trim the line to look like a video file (if it is a pose file)
+    VIDEO_FILE=$(sed -E 's:(_pose_est_v[0-9]+)?\.(avi|h5):.avi:' <(echo ${BATCH_LINE}))
 
     # the "v1" is for output format versioning. If format changes this should be updated
     OUT_DIR="${VIDEO_FILE%.*}_behavior/v1"
 
     cd "$(dirname "${BATCH_FILE}")"
 
-    POSE_FILE_V4="${VIDEO_FILE%.*}_pose_est_v4.h5"
-    POSE_FILE="${POSE_FILE_V4}"
+    POSE_FILE=$(find_pose_file ${BATCH_LINE})
 
     if [[ ! ( -f "${POSE_FILE}" ) ]]
     then
-        POSE_FILE_V3="${VIDEO_FILE%.*}_pose_est_v3.h5"
-        POSE_FILE="${POSE_FILE_V3}"
-    fi
-
-    if [[ ! ( -f "${POSE_FILE}" ) ]]
-    then
-        POSE_FILE_V2="${VIDEO_FILE%.*}_pose_est_v2.h5"
-        POSE_FILE="${POSE_FILE_V2}"
-    fi
-
-    if [[ ! ( -f "${POSE_FILE}" ) ]]
-    then
-        echo "ERROR: failed to find pose file for ${VIDEO_FILE}" >&2
+        echo "ERROR: failed to find pose file for ${BATCH_LINE}" >&2
         exit 1
     fi
 
     echo "DUMP OF CURRENT ENVIRONMENT:"
     env
-    echo "BEGIN PROCESSING: ${POSE_FILE} for ${VIDEO_FILE}"
+    echo "BEGIN PROCESSING: ${POSE_FILE} for ${BATCH_LINE} (${POSE_FILE}"
     module load singularity
-    singularity run "${ROOT_DIR}/behavior-classifier.sif" classify --xgboost --training "${CLASSIFIER_FILE}" --input-pose "${POSE_FILE}" --out-dir "${OUT_DIR}"
+    singularity run "${CLASSIFICATION_IMG}" classify --training "${CLASSIFIER_FILE}" --input-pose "${POSE_FILE}" --out-dir "${OUT_DIR}"
 
     echo "FINISHED PROCESSING: ${POSE_FILE}"
 fi
