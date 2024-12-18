1208b4c199383c2af31629cc1f1ad60fc22afed2
Brian Geuther <Brian.Geuther@jax.org>
2024-12-18 10:59:33 -0500

    Updating a handful of segmentation tests

--- tests/feature_tests/test_hu_moments.py
+++ tests/feature_tests/test_hu_moments.py
@@ -8,16 +8,11 @@ from src.jabs.feature_extraction.segmentation_features import HuMoments
 class Test(SBC, unittest.TestCase):
     """ This test will provide testing coverage for the HuMoments Feature class. """
 
-    pixel_scale = 1.0
-
     def testHuMomentFeatureName(self) -> None:
         """ Test HuMoment class. """
 
         # test that data was read and setup correctly
-        seg_data = self._pose_est_v6._segmentation_dict["seg_data"]
-        assert sum(self.seg_data.shape) == sum(seg_data.shape)
-
-        huMomentsFeature = HuMoments(self._pose_est_v6, self.pixel_scale)
+        huMomentsFeature = self.feature_mods['hu_moments']
         
         assert huMomentsFeature._feature_names[-2] == "hu6"
 
@@ -25,7 +20,7 @@ class Test(SBC, unittest.TestCase):
 
         huMoments_by_frame = huMomentsFeature.per_frame(i)
 
-        assert huMoments_by_frame.shape[1] == 7
+        assert len(huMoments_by_frame) == 7
 
         
 
