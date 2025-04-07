ec7e682621d1a081211d0ed4909bc1ce155d021f
Glen Beane <356266+gbeane@users.noreply.github.com>
2025-04-07 19:22:26 -0400

    Update setup_windows.bat to also support python 3.11 and 3.12

--- setup_windows.bat
+++ setup_windows.bat
@@ -7,23 +7,39 @@ cd /d "%~dp0"
 
 REM discontinue support for this script once we are delivering wheels for installing JABS with pip
 
-:: Check for Python Installation
-echo Checking for python
-for /f "tokens=*" %%i in ('python --version 2^>nul') do set VER=%%i
-
-SET OK=0
-
-:: Supported versions of Python
-if "%VER:~7,3%"=="3.10" SET OK=1
-
-if %OK% == 1 (
-  echo Found %VER%
-  echo Setting up Python Virtualenv...
-  python -m venv jabs.venv
-  jabs.venv\Scripts\activate.bat && pip install .
-) else (
-  echo JABS Requires Python 3.10
+REM Check for skip version check argument
+set "SKIP_VERSION_CHECK=0"
+for %%i in (%*) do (
+    if "%%i"=="--skip-version-check" set "SKIP_VERSION_CHECK=1"
 )
+
+if %SKIP_VERSION_CHECK%==0 (
+    :: Check for Python Installation
+    echo Checking for python
+    for /f "tokens=*" %%i in ('python --version 2^>nul') do set VER=%%i
+
+    SET OK=0
+
+    :: Supported versions of Python
+    if "%VER:~7,3%"=="3.10" SET OK=1
+    if "%VER:~7,3%"=="3.11" SET OK=1
+    if "%VER:~7,3%"=="3.12" SET OK=1
+
+    if %OK% == 1 (
+        echo Found %VER%
+    ) else (
+        echo JABS Requires Python 3.10, 3.11, or 3.12
+        REM restore working directory
+        cd /d "%initialDir%"
+        exit /b 1
+    )
+) else (
+    echo Skipping Python version check
+)
+
+echo Setting up Python Virtualenv...
+python -m venv jabs.venv
+jabs.venv\Scripts\activate.bat && pip install .
 
 REM restore working directory
 cd /d "%initialDir%"
