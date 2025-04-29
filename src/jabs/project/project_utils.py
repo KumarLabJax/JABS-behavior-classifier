10c29ecded737ff1fddead448f8629df1994139d
Glen Beane <356266+gbeane@users.noreply.github.com>
2025-04-29 15:12:29 -0400

    add return and raises documentation to to_safe_name docstring

--- src/jabs/project/project_utils.py
+++ src/jabs/project/project_utils.py
@@ -5,9 +5,10 @@ import h5py
 
 def to_safe_name(behavior: str) -> str:
     """
-    Create a version of the given behavior name that
-    should be safe to use in filenames.
+    Create a version of the given behavior name that should be safe to use in filenames.
     :param behavior: string behavior name
+    :returns: sanitized behavior name
+    :raises ValueError: if the behavior name is empty after sanitization
     """
 
     safe_behavior = re.sub(r"[^\w.-]+", "_", behavior, flags=re.UNICODE)
