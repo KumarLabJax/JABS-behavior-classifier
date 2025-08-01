82794d12dce7b5fe0f20abc34119dfef9aac4779
Glen Beane <356266+gbeane@users.noreply.github.com>
2025-07-31 23:38:59 -0400

    Merge FrameWidget with FrameWidgetWithInteractiveOverlays

--- src/jabs/ui/player_widget/frame_with_control_overlay.py
+++ src/jabs/ui/player_widget/frame_with_control_overlay.py
@@ -1,35 +1,37 @@
 import enum
 
+import numpy as np
 from intervaltree import IntervalTree
-from PySide6 import QtCore, QtGui
+from PySide6 import QtCore, QtGui, QtWidgets
 
 from jabs.pose_estimation import PoseEstimation
 
-from .frame_widget import FrameWidget
 from .overlays.annotation_overlay import AnnotationOverlay
 from .overlays.control_overlay import ControlOverlay
 from .overlays.floating_id_overlay import FloatingIdOverlay
+from .overlays.label_overlay import LabelOverlay
 from .overlays.overlay import Overlay
 from .overlays.pose_overlay import PoseOverlay
 
 
-class FrameWidgetWithInteractiveOverlays(FrameWidget):
+class FrameWidgetWithInteractiveOverlays(QtWidgets.QLabel):
     """
-    A `FrameWidget` subclass that adds an interactive overlays.
+    A Video Frame with interactive overlays.
 
     This widget displays a number of interactive overlays on top of the video frame, including
-     * a controls overlay, which is displayed when the mouse is over the frame pixmap area.
-     * an overlay for displaying timeline annotations for the current frame.
+      * a controls overlay, which is displayed when the mouse is over the frame pixmap area.
+      * an overlay for displaying timeline annotations for the current frame.
+      * an overlay for displaying pose estimation keypoints and skeletons.
+      * an overlay for displaying identity labels, which can be in different modes.
+      * an overlay for displaying labels next to each mouse, which can be manual labels or predictions.
 
     Signals:
         playback_speed_changed (float): Emitted when the playback speed is changed by the user.
+        id_label_clicked (int): Emitted when an identity label is clicked, passing the identity index.
+        pixmap_clicked (dict): Emitted when the user clicks on the pixmap, passing the x and y coordinates of the click.
 
     Implements some additional properties and methods so that overlays can access
     information from the frame widget.
-
-    Todo:
-        - Merge FrameWidget and FrameWidgetWithInteractiveOverlays into a single class, and
-          implement the centroid & minimalist identity overlay and label overlay as Overlay subclasses.
     """
 
     class PoseOverlayMode(enum.IntEnum):
@@ -39,31 +41,53 @@ class FrameWidgetWithInteractiveOverlays(FrameWidget):
         ACTIVE_IDENTITY = enum.auto()
         NONE = enum.auto()
 
+    class IdentityOverlayMode(enum.IntEnum):
+        """Enum for identity overlay options."""
+
+        MINIMAL = enum.auto()
+        CENTROID = enum.auto()
+        FLOATING = enum.auto()
+
+    pixmap_clicked = QtCore.Signal(dict)
     playback_speed_changed = QtCore.Signal(float)
     id_label_clicked = QtCore.Signal(int)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy.Policy.Ignored, QtWidgets.QSizePolicy.Policy.Ignored
+        )
+        self.setMinimumSize(400, 400)
         self.setMouseTracking(True)
+
+        self._scaled_pix_x = 0
+        self._scaled_pix_y = 0
+        self._scaled_pix_width = 0
+        self._scaled_pix_height = 0
+        self._frame_number = 0
+        self._active_identity = 0
+        self._pose: PoseEstimation | None = None
         self._annotations: IntervalTree | None = None
+        self._labels: list[np.ndarray] | None = None
+
+        self._pose_overlay_mode = self.PoseOverlayMode.NONE
+        self._id_overlay_mode = self.IdentityOverlayMode.FLOATING
 
         # initialize overlays
         self._control_overlay = ControlOverlay(self)
         self._control_overlay.playback_speed_changed.connect(self.playback_speed_changed)
         self._annotation_overlay = AnnotationOverlay(self)
-        self._annotation_overlay.enabled = True
-        self._floating_id_overlay = FloatingIdOverlay(self)
-        self._floating_id_overlay.enabled = True
-        self._floating_id_overlay.id_label_clicked.connect(self.id_label_clicked)
-        pose_overlay = PoseOverlay(self)
-        pose_overlay.enabled = True
+        floating_id_overlay = FloatingIdOverlay(self)
+        floating_id_overlay.id_label_clicked.connect(self.id_label_clicked)
 
         # overlays are listed in the order they should be painted
         # an overlay on top of another overlay will have priority when handling click events
         self.overlays: list[Overlay] = [
-            pose_overlay,
+            PoseOverlay(self),
+            LabelOverlay(self),
             self._annotation_overlay,
-            self._floating_id_overlay,
+            floating_id_overlay,
             # the control overlay is painted last, so it is on top of all other overlays
             self._control_overlay,
         ]
@@ -81,31 +105,18 @@ class FrameWidgetWithInteractiveOverlays(FrameWidget):
             self.update()
 
     @property
-    def floating_id_overlay_enabled(self) -> bool:
-        """Get whether the floating ID overlay is enabled."""
-        return self._floating_id_overlay.enabled
-
-    @floating_id_overlay_enabled.setter
-    def floating_id_overlay_enabled(self, enabled: bool) -> None:
-        """Set whether the floating ID overlay is enabled."""
-        if self._floating_id_overlay.enabled != enabled:
-            self._floating_id_overlay.enabled = enabled
-            self.update()
-
-    @property
-    def identity_overlay_mode(self) -> FrameWidget.IdentityOverlayMode:
+    def identity_overlay_mode(self) -> IdentityOverlayMode:
         """Get the current identity overlay mode."""
-        return super().identity_overlay_mode
+        return self._id_overlay_mode
 
     @identity_overlay_mode.setter
-    def identity_overlay_mode(self, mode: FrameWidget.IdentityOverlayMode) -> None:
+    def identity_overlay_mode(self, mode: IdentityOverlayMode) -> None:
         """Set the identity overlay mode for the frame widget.
 
         Args:
             mode (IdentityOverlayMode): The mode to set for overlaying identities.
         """
         if self._id_overlay_mode != mode:
-            self.floating_id_overlay_enabled = mode == FrameWidget.IdentityOverlayMode.FLOATING
             self._id_overlay_mode = mode
             self.update()
 
@@ -175,18 +186,167 @@ class FrameWidgetWithInteractiveOverlays(FrameWidget):
         """Returns the currently active identity index, or None if no identity is active."""
         return self._active_identity
 
+    @property
+    def labels(self) -> list[np.ndarray] | None:
+        """Returns the label values for overlaying on the frame."""
+        return self._labels
+
+    def reset(self) -> None:
+        """reset state of frame widget"""
+        self._scaled_pix_x = 0
+        self._scaled_pix_y = 0
+        self._scaled_pix_width = 0
+        self._scaled_pix_height = 0
+        self._pose = None
+        self._frame_number = 0
+        self._active_identity = 0
+        self.clear()
+
+    def convert_identity_to_external(self, identity: int) -> int:
+        """Convert an internal identity index to an external identity index.
+
+        This is useful when the pose estimation uses external identities so that we can display
+        the external identity instead of the internal jabs identity index.
+        """
+        if self._pose and self._pose.external_identities:
+            try:
+                return self._pose.external_identities[identity]
+            except IndexError:
+                # If the identity is not found in external identities, fall through to return the original identity
+                pass
+        return identity
+
+    def set_pose(self, pose: PoseEstimation) -> None:
+        """Set the pose estimation for the frame widget.
+
+        Pose is used to annotate the frame, this method muse be called any time the
+        a new video is loaded.
+        """
+        self._pose = pose
+
+    def set_active_identity(self, identity: int) -> None:
+        """Set the active identity for the frame widget.
+
+        This identity will be highlighted in the frame.
+        """
+        self._active_identity = identity
+
+    def set_label_overlay(self, labels: list[np.ndarray]) -> None:
+        """set label values to use for overlaying on the frame.
+
+        Labels are used to indicated behavior or not behavior label or prediction for each identity
+
+        Args:
+            labels (list[np.ndarray]): list of label arrays, one for each identity.
+            Each array should contain the label for each frame in the video for the given identity.
+        """
+        self._labels = labels
+
+    def update_frame(self, frame: QtGui.QImage, frame_number: int) -> None:
+        """Update the frame displayed in the widget."""
+        self._frame_number = frame_number
+        self.setPixmap(QtGui.QPixmap.fromImage(frame))
+
+    def _widget_to_image_coords(self, x: int, y: int) -> tuple[int, int]:
+        """Convert the given x, y coordinates from FrameWidget coordinates to correct image coordinates.
+
+        Ie which pixel did the user click on? We account for image scaling and translation
+        """
+        pixmap = self.pixmap()
+        if (
+            pixmap is not None
+            and (
+                self._scaled_pix_height != pixmap.height()
+                or self._scaled_pix_width != pixmap.width()
+                or self._scaled_pix_x != 0
+                or self._scaled_pix_y != 0
+            )
+            and self._scaled_pix_width >= 1
+            and self._scaled_pix_height >= 1
+        ):
+            # we've done all the checks and it's safe to transform the x, y point
+            x -= self._scaled_pix_x
+            y -= self._scaled_pix_y
+            x *= pixmap.width() / self._scaled_pix_width
+            y *= pixmap.height() / self._scaled_pix_height
+
+        return x, y
+
+    def image_to_widget_coords(self, pix_x: int, pix_y: int) -> tuple[int, int]:
+        """Convert true image coordinates to FrameWidget (QLabel) coordinates
+
+        Accounts for scaling and centering to fit image in FrameWidget.
+
+        Args:
+            pix_x (int): x coordinate in pixmap coordinates
+            pix_y (int): y coordinate in pixmap coordinates
+
+        Returns:
+            tuple[int, int]: x, y coordinates in FrameWidget coordinates
+        """
+        pixmap = self.pixmap()
+        if pixmap is not None and self._scaled_pix_width >= 1 and self._scaled_pix_height >= 1:
+            # Scale pixmap coordinates to scaled pixmap size
+            x = pix_x * self._scaled_pix_width / pixmap.width()
+            y = pix_y * self._scaled_pix_height / pixmap.height()
+
+            # Offset by the scaled pixmap's position in the widget
+            x += self._scaled_pix_x
+            y += self._scaled_pix_y
+            return int(x), int(y)
+        return pix_x, pix_y
+
+    def sizeHint(self) -> QtCore.QSize:
+        """Override QLabel.sizeHint to give an initial starting size."""
+        return QtCore.QSize(1024, 1024)
+
     def paintEvent(self, event: QtGui.QPaintEvent) -> None:
         """Handles the paint event for the widget and draws all overlays.
 
         Args:
             event (QtGui.QPaintEvent): The paint event containing region to be updated.
         """
-        super().paintEvent(event)
-        painter = QtGui.QPainter(self)
-        painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing, True)
-        for overlay in self.overlays:
-            overlay.paint(painter)
-        painter.end()
+        # only draw if we have an image to show
+        if not self.pixmap().isNull():
+            # current size of the widget
+            size = self.size()
+
+            painter = QtGui.QPainter(self)
+            painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing, True)
+            painter.setRenderHint(QtGui.QPainter.RenderHint.TextAntialiasing, True)
+            point = QtCore.QPoint(0, 0)
+
+            # scale the image to the current size of the widget.
+            pix = self.pixmap().scaled(
+                size,
+                QtCore.Qt.AspectRatioMode.KeepAspectRatio,
+                QtCore.Qt.TransformationMode.SmoothTransformation,
+            )
+
+            # because we are maintaining aspect ratio, the scaled frame might
+            # not be the same dimensions as the area we are painting it.
+            # adjust the start point to center the image in the widget
+            point.setX((size.width() - pix.width()) // 2)
+            point.setY((size.height() - pix.height()) // 2)
+
+            # draw the pixmap starting at the new calculated offset
+            painter.drawPixmap(point, pix)
+
+            # save the scaled pixmap dimensions for use by the mousePressEvent and _overlay_identities methods
+            self._scaled_pix_x = point.x()
+            self._scaled_pix_y = point.y()
+            self._scaled_pix_width = pix.width()
+            self._scaled_pix_height = pix.height()
+
+            # paint all overlays in the order they were added
+            for overlay in self.overlays:
+                overlay.paint(painter)
+            painter.end()
+
+        else:
+            # if we don't have a pixmap to display just call the original QLabel
+            # paintEvent
+            super().paintEvent(event)
 
     def mouseMoveEvent(self, event) -> None:
         """Handles mouse move events and delegates them to all overlays.
@@ -208,11 +368,11 @@ class FrameWidgetWithInteractiveOverlays(FrameWidget):
         for overlay in self.overlays:
             overlay.handle_leave(event)
 
-    def mousePressEvent(self, event) -> None:
-        """Handles mouse press events and delegates them to all overlays.
+    def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
+        """Process mousePressEvent.
 
-        Args:
-            event (QtGui.QMouseEvent): The mouse press event.
+        Let overlays have a chance to handle click first, if none handle the
+        click then emit a signal with the clicked pixel coordinates.
         """
         handled = False
         for overlay in self.overlays:
@@ -220,7 +380,8 @@ class FrameWidgetWithInteractiveOverlays(FrameWidget):
                 handled = True
                 break
         if not handled:
-            super().mousePressEvent(event)
+            pix_x, pix_y = self._widget_to_image_coords(event.x(), event.y())
+            self.pixmap_clicked.emit({"x": pix_x, "y": pix_y})
 
     def eventFilter(self, obj: QtCore.QObject, event: QtCore.QEvent) -> bool:
         """Filters events before they reach the target object and delegates to overlays.
