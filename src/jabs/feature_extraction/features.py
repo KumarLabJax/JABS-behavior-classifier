72d32a945b097ee7426404d37f94b9e244161b62
Glen Beane <356266+gbeane@users.noreply.github.com>
2025-08-13 10:11:02 -0400

    revert name change of window op for circular features

--- src/jabs/feature_extraction/features.py
+++ src/jabs/feature_extraction/features.py
@@ -16,7 +16,7 @@ from .landmark_features import LandmarkFeatureGroup
 from .segmentation_features import SegmentationFeatureGroup
 from .social_features import SocialFeatureGroup
 
-FEATURE_VERSION = 14
+FEATURE_VERSION = 15
 
 _FEATURE_MODULES = [BaseFeatureGroup, SocialFeatureGroup, SegmentationFeatureGroup]
 
