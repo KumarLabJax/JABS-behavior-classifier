e3a23f00428eaddeec1cf70fdbd0a742aea21524
Glen Beane <356266+gbeane@users.noreply.github.com>
2025-09-17 08:48:29 -0400

    add version for serialized annotation JSON file

--- src/jabs/project/video_labels.py
+++ src/jabs/project/video_labels.py
@@ -9,6 +9,9 @@ if TYPE_CHECKING:
     from .project_merge import MergeStrategy
 
 
+SERIALIZED_VERSION = 1
+
+
 class VideoLabels:
     """Stores and manages frame-level behavior labels for each identity in a video.
 
@@ -168,6 +171,7 @@ class VideoLabels:
 
         """
         label_dict: dict[str, Any] = {
+            "version": SERIALIZED_VERSION,
             "file": self._filename,
             "num_frames": self._num_frames,
             "labels": {},
