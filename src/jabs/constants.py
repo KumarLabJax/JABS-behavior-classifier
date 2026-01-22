c5f8d21e269374895b5c9220a66dcae46bcaf734
Glen Beane <356266+gbeane@users.noreply.github.com>
2026-01-22 10:31:32 -0500

    updating constants.py

--- src/jabs/constants.py
+++ src/jabs/constants.py
@@ -2,9 +2,9 @@ ORG_NAME = "JAX"
 APP_NAME = "JABS"
 APP_NAME_LONG = f"{ORG_NAME} Animal Behavior System"
 
-# a hard coded random seed used for the final training done with all
-# training data before saving the classifier
-# the choice of random seed is arbitrary
+# a hard coded random seed used for the final training
+# This is not used during cross-validation, but to ensure that final classifier is reproducible
+# we use this fixed seed when training the final model after cross validation.
 FINAL_TRAIN_SEED = 0xAB3BDB
 
 # some defaults for compressing hdf5 output
@@ -19,5 +19,4 @@ LICENSE_ACCEPTED_KEY = "license_accepted"
 LICENSE_VERSION_KEY = "license_version"
 RECENT_PROJECTS_KEY = "recent_projects"
 SESSION_TRACKING_ENABLED_KEY = "session_tracking_enabled"
-TORCH_BACKEND_KEY = "torch_backend"
 WINDOW_SIZE_KEY = "main_window_size"
